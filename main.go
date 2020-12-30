7f82c3dbc40dc3e0945d9f964be3632a21a01338
Shou Tseng <yakushou730@gmail.com>
2020-12-30 21:43:29 +0800

    Register routers

--- main.go
+++ main.go
@@ -1,13 +1,21 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
+	"net/http"
+	"time"
+
+	"github.com/yakushou730/blog-service/internal/routers"
 )
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{"message": "pong"})
-	})
-	r.Run()
+	router := routers.NewRouter()
+	s := &http.Server{
+		Addr:              ":8080",
+		Handler:           router,
+		ReadTimeout:       10 * time.Second,
+		ReadHeaderTimeout: 10 * time.Second,
+		MaxHeaderBytes:    1 << 20,
+	}
+
+	s.ListenAndServe()
 }
