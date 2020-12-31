3bfddbf29c299ffa3a7628970994e657c0af0fea
Shou Tseng <yakushou730@gmail.com>
2020-12-31 12:48:22 +0800

    Load Config

---
+++ global/setting.go
@@ -0,0 +1,9 @@
+package global
+
+import "github.com/yakushou730/blog-service/pkg/setting"
+
+var (
+	ServerSetting   *setting.ServerSettings
+	AppSetting      *setting.AppSettings
+	DatabaseSetting *setting.DatabaseSettings
+)
--- main.go
+++ main.go
@@ -1,21 +1,56 @@
 package main
 
 import (
+	"log"
 	"net/http"
 	"time"
 
+	"github.com/gin-gonic/gin"
+	"github.com/yakushou730/blog-service/global"
 	"github.com/yakushou730/blog-service/internal/routers"
+	"github.com/yakushou730/blog-service/pkg/setting"
 )
 
+func init() {
+	err := SetupSetting()
+	if err != nil {
+		log.Fatalf("init.setupSetting err: %v", err)
+	}
+}
+
 func main() {
+	gin.SetMode(global.ServerSetting.RunMode)
 	router := routers.NewRouter()
 	s := &http.Server{
-		Addr:              ":8080",
+		Addr:              ":" + global.ServerSetting.HttpPort,
 		Handler:           router,
-		ReadTimeout:       10 * time.Second,
-		ReadHeaderTimeout: 10 * time.Second,
+		ReadTimeout:       global.ServerSetting.ReadTimeout,
+		ReadHeaderTimeout: global.ServerSetting.WriteTimeout,
 		MaxHeaderBytes:    1 << 20,
 	}
 
 	s.ListenAndServe()
+}
+
+func SetupSetting() error {
+	systemSetting, err := setting.NewSetting()
+	if err != nil {
+		return err
+	}
+	err = systemSetting.ReadSection("Server", &global.ServerSetting)
+	if err != nil {
+		return err
+	}
+	err = systemSetting.ReadSection("App", &global.AppSetting)
+	if err != nil {
+		return err
+	}
+	err = systemSetting.ReadSection("Database", &global.DatabaseSetting)
+	if err != nil {
+		return err
+	}
+	global.ServerSetting.ReadTimeout *= time.Second
+	global.ServerSetting.WriteTimeout *= time.Second
+
+	return nil
 }
